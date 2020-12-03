c894e985c16369d659cd787ac1c27ae8e52eeb9c
Olivia Toolson <livytoolson@gmail.com>
2020-12-03 10:32:38 -0800

    wip

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 4000
+const path = require('path');
+const { send } = require('process');
 
 console.log('Deploying an API');
 console.log(__dirname);
@@ -21,10 +23,16 @@ console.log(process.env.PORT);
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // fake API, routers
 app.use('/api/*', (_, res) => {
     res.json({ data: 'Deploying an API' })
+});
+
+app.use('*', (_, res) => {
+    // send back index.html
+    res.sendFile(path.join(__dirname, 'cllient/build', 'index.html'))
 })
 
 app.listen(port, () => {
